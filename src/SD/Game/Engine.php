de19d790bf6edc2382d9a3e949269a4392f0e221
rbunce <richardallenbunce@gmail.com>
2014-08-13 16:28:38 -0700

    Upped framerate to 60 fps

--- src/SD/Game/Engine.php
+++ src/SD/Game/Engine.php
@@ -18,12 +18,8 @@ use SD\InvadersBundle\Event\GameOverEvent;
  */
 class Engine
 {
-    /**
-     * @var int
-     */
-    const HEARTBEAT_DURATION = 20000;
     
-    const FRAMES_PER_SEC = 30;
+    const FRAMES_PER_SEC = 60;
     
     const ONE_SEC_MICRO = 1000000;
 
