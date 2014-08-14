ca8b5d9663ca920d4d7fc0cb74e167063346d027
Scott Driscoll <scott.driscoll@opensoftdev.com>
2014-08-14 10:34:19 -0700

    Fix keyboard input lag

--- src/SD/Game/Keyboard.php
+++ src/SD/Game/Keyboard.php
@@ -23,17 +23,19 @@ class Keyboard
     /**
      * @var string
      */
-    const RIGHT_ARROW = 'C';
+    const RIGHT_ARROW = '→';
 
     /**
      * @var string
      */
-    const LEFT_ARROW = 'D';
+    const LEFT_ARROW = '←';
 
     /**
      * @var string
      */
     const FIRE_KEY = ' ';
+
+    const CONTROL_KEY = '';
 
     /**
      * @var EventDispatcherInterface
@@ -59,7 +61,7 @@ class Keyboard
      */
     public function processKeyboardEvents(HeartbeatEvent $event)
     {
-        if (($key = $this->nonblockingRead()) !== null) {
+        if (($key = $this->readKey()) !== null) {
             switch ($key) {
                 case self::LEFT_ARROW:
                     $this->eventDispatcher->dispatch(Events::PLAYER_MOVE_LEFT, new PlayerMoveLeftEvent());
@@ -79,7 +81,7 @@ class Keyboard
     /**
      * Reads from a stream without waiting for a \n character.
      *
-     * @return string
+     * @return string|null
      */
     private function nonblockingRead()
     {
@@ -93,5 +95,34 @@ class Keyboard
         }
 
         return stream_get_line(STDIN, 1);
+    }
+
+    /**
+     * @return string|null
+     */
+    private function readKey()
+    {
+        $key = $this->nonblockingRead();
+        if (null === $key) {
+            return null;
+        }
+
+        if ($key == self::CONTROL_KEY) {
+            // throw away next character
+            $this->nonblockingRead();
+            switch ($this->nonblockingRead()) {
+                case 'C':
+                    $key = self::RIGHT_ARROW;
+                    break;
+                case 'D':
+                    $key = self::LEFT_ARROW;
+                    break;
+                default:
+                    $key = null;
+                    break;
+            }
+        }
+
+        return $key;
     }
 }
